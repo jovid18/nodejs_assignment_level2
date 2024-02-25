ad1efb9207a6204bab54ca380748a1f02e6f5089
jovid_18 <cshcho99@gmail.com>
2024-02-25 23:09:06 +0900

    FEAT: download packages, add folders and files, write app.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+// import CommentsRouter from "./routes/comments.router.js";
+// import ReviewsRouter from "./routes/reviews.router.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(PORT, "포트로 서버가 열렸어요!");
+});
